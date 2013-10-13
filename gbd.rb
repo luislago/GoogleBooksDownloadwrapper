987f0385b387a76609fcedb9d52d2557ed170ada
Luís Lago <luisalveslago@gmail.com>
2013-10-13 10:33:49 +0100

    first commit

---
+++ gbd.rb
@@ -0,0 +1,25 @@
+require 'rubygems'
+require 'open-uri'
+require 'zip'
+
+
+puts "link"
+link = gets.chomp
+puts "Nome"
+DATA_DIR = gets.chomp 
+Dir.mkdir(DATA_DIR) unless File.exists?(DATA_DIR)
+#Uses a .jar file to download pages from google books
+`java -jar GoogleBooksDownloader.jar "#{link}"  "/home/luis/googlebooksdownloader/#{DATA_DIR}"`
+
+#creates a .zip file
+
+issue_dirzip = DATA_DIR + "/"
+ 
+ zipfile_name = "/home/luis/googlebooksdownloader/" + DATA_DIR + ".zip"
+Zip::File.open(zipfile_name, Zip::File::CREATE) do |zipfile|
+    Dir[File.join(issue_dirzip, '**', '**')].each do |file|
+      zipfile.add(file.sub(issue_dirzip, ''), file)
+    end
+end
+
+puts "done"
